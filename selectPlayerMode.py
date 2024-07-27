6db42dd7a7c38b0d9a43cb7ab81c9cf2fcc18d66
LamaV10 <lama.customs@gmail.com>
2024-07-27 20:34:57 +0200

    selectPlayerMode.py update. Now you can choose between us or de layout, because if you have another layout than us set on your machine, pyautogui is going to write some gibberish.

--- selectPlayerMode.py
+++ selectPlayerMode.py
@@ -1,14 +1,29 @@
 import pyautogui as auto
 import time 
 
+language = int(input("US (1) or DE (2) layout: "))
 mode = int(input("singleplayer (1) or multiplayer(2): "))
 print(mode)
 
-if mode == 1:
+if mode == 1 and language == 1:
+    #this is the normal textthe normal text for the us layout
     auto.write("python PySTK_main.py")
     time.sleep(1)
     auto.press('enter')
+if mode == 1 and language == 2:
+    #this is for the de layout
+    auto.write("pzthon PzSTK?main.pz")
+    time.sleep(1)
+    auto.press('enter')
 
-if mode == 2:
+
+if mode == 2 and language == 1:
+    #this is the normal textthe normal text for the us layout
     auto.write("python PySTK_main_duo.py")
+    time.sleep(1)
+    auto.press('enter')
+if mode == 2 and language == 2:
+    #this is for the de layout
+    auto.write("pzthon PzSTK?main?duo.pz")
+    time.sleep(1)
     auto.press('enter')
