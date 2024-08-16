0cd7662ecad031a5ef424d74367b6193bdc3b0a8
LamaV10 <lama.customs@gmail.com>
2024-08-16 20:48:19 +0200

    Updated requirements.txt.

--- selectPlayerMode.py
+++ selectPlayerMode.py
@@ -1,68 +1,100 @@
-import pyautogui as auto
+# import pyautogui as auto
 import time 
 import pygame
 import pygame_gui
 from utils import scale_image, blit_rotate_center
 
-scale_factor = float(input("Choose scale-factor: "))
+# scale_factor = float(input("Choose scale-factor: "))
 
-Background = scale_image(pygame.image.load("imgs/Background/selMod_BG/selMod_bg.png"), scale_factor * 0.5 )
+# Background = scale_image(pygame.image.load("imgs/Background/selMod_BG/selMod_bg.png"), scale_factor * 0.5 )
 
 #window setup
-WIDTH, HEIGHT = Background.get_width(), Background.get_height()
+# WIDTH, HEIGHT = Background.get_width(), Background.get_height()
+WIDTH, HEIGHT = 1600, 900
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("select PySTK Mode")
 
-MANAGER = pygame_gui.UIManager((WIDTH, HEIGHT))
-TEXT_INPUT = pygame_gui.elements.UITextEntryLine(relative_rect=pygame.REct((350, 275), (900, 50)), manager=MANAGER, object_id="#main_text_entry")
+manager = pygame_gui.UIManager((1600, 900))
 
-run = True
-#loop
-while run:
-    UI_REFRESH_RATE = CLOCK.tick(60)/1000
+TEXT_INPUT = pygame_gui.elements.UITextEntryLine(relative_rect=pygame.Rect((350, 275), (900, 50)), manager=manager, object_id="#main_text_entry")
 
-    #cloeses the windows if run = False
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            run = False
-        MANAGER.process_events(event)
-    
-    MANAGER.update(UI_REFRESH_RATE)
-    MANAGER.draw_ui(WIN)
+def get_scale_factor():
+    run = True
+    while run:
+        UI_REFRESH_RATE = CLOCK.tick(60)/1000
 
-    # Using blit to copy content from one surface to other
-    WIN.blit(Background, (0, 0))
-    pygame.display.update()
+        #cloeses the windows if run = False
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+            # if (event.type == pygame_gui.UI_TEXT_ENTRY_FINISHED and
+            #     event.ui_object_id == '#main_text_entry'):
+            #
+            manager.process_events(event)
+        
+        manager.update(UI_REFRESH_RATE)
 
-    print("What layout is currently set on your machine?")
-    print("(This is not relevant if you are on Windows. Choose US in this case!)")
-    language = int(input("US (1) or DE (2) layout: "))
-    mode = int(input("singleplayer (1) or multiplayer(2): "))
-    print(mode)
+        SCREEN.fill("white")
 
-    if mode == 1 and language == 1:
-        #this is the normal textthe normal text for the us layout
-        auto.write("python PySTK_main.py")
-        time.sleep(1)
-        auto.press('enter')
-    if mode == 1 and language == 2:
-        #this is for the de layout
-        auto.write("pzthon PzSTK?main.pz")
-        time.sleep(1)
-        auto.press('enter')
+        manager.draw_ui(SCREEN)
+
+        pygame.display.update()
+ 
 
 
-    if mode == 2 and language == 1:
-        #this is the normal textthe normal text for the us layout
-        auto.write("python PySTK_main_duo.py")
-        time.sleep(1)
-        auto.press('enter')
-    if mode == 2 and language == 2:
-        #this is for the de layout
-        auto.write("pzthon PzSTK?main?duo.pz")
-        time.sleep(1)
-        auto.press('enter')
-    
-    run = False
 
-pygame.quit()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+######################################################################################
+
+    # print("What layout is currently set on your machine?")
+    # print("(This is not relevant if you are on Windows. Choose US in this case!)")
+    # language = int(input("US (1) or DE (2) layout: "))
+    # mode = int(input("singleplayer (1) or multiplayer(2): "))
+    # print(mode)
+    #
+    # if mode == 1 and language == 1:
+    #     #this is the normal textthe normal text for the us layout
+    #     auto.write("python PySTK_main.py")
+    #     time.sleep(1)
+    #     auto.press('enter')
+    # if mode == 1 and language == 2:
+    #     #this is for the de layout
+    #     auto.write("pzthon PzSTK?main.pz")
+    #     time.sleep(1)
+    #     auto.press('enter')
+    #
+    #
+    # if mode == 2 and language == 1:
+    #     #this is the normal textthe normal text for the us layout
+    #     auto.write("python PySTK_main_duo.py")
+    #     time.sleep(1)
+    #     auto.press('enter')
+    # if mode == 2 and language == 2:
+    #     #this is for the de layout
+    #     auto.write("pzthon PzSTK?main?duo.pz")
+    #     time.sleep(1)
+    #     auto.press('enter')
+    #
+    # run = False
+
+# pygame.quit()
