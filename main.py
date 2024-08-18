e99dae2542175c3c102e1179faa0bf571ff516ea
LamaV10 <lama.customs@gmail.com>
2024-08-18 21:30:38 +0200

    Updated requirements.txt. The GUI from main.py is still in the works!

--- main.py
+++ main.py
@@ -1,11 +1,55 @@
+import pygame
 import time
 import pyautogui as auto
+import pygame_gui
+import sys
+
+pygame.init()
+
+WIDTH, HEIGHT = 1600, 900
+SCREEN = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Text Input in PyGame | BaralTech")
+
+manager = pygame_gui.UIManager((1600, 900))
+
+text_input = pygame_gui.elements.UITextEntryLine(relative_rect=pygame.Rect((350, 275), (900, 50)), manager=manager,
+                                               object_id='#main_text_entry')
+
+language = 0
+
+clock = pygame.time.Clock()
+def get_user_name():
+    run = True
+    global language
+    while run:
+        UI_REFRESH_RATE = clock.tick(60)/1000
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            if (event.type == pygame_gui.UI_TEXT_ENTRY_FINISHED and
+                event.ui_object_id == '#main_text_entry'):
+                language = event.text 
+                print(language)
+                run = False
+            
+            manager.process_events(event)
+
+        manager.update(UI_REFRESH_RATE)
+
+        SCREEN.fill("black")
+
+        manager.draw_ui(SCREEN)
+
+        pygame.display.update()
+    
+
+get_user_name()
 
 
 print("What layout is currently set on your machine?")
 print("(This is not relevant if you are on Windows. Choose US in this case!)")
 
-language = int(input("US (1) or DE (2) layout: "))
 mode = int(input("singleplayer (1) or multiplayer(2): "))
 
 if language == 1:
