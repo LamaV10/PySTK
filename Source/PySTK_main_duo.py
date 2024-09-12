0d8c4e464ba9fbbe319f0427cdc2ad9e39d106f1
LamaV10 <lama.customs@gmail.com>
2024-09-12 14:50:44 +0200

    Fixed spacing on the duo version.

--- Source/PySTK_main_duo.py
+++ Source/PySTK_main_duo.py
@@ -2,13 +2,14 @@
 import pygame
 import time
 import math
+
 import music
+music.music()
+
 from utils import scale_image, blit_rotate_center
 pygame.init()
 
 #Bruno Banani
-
-music.music()
 
 #choose if you want to play on 85 or 144
 #85 FPS is easier to play and works great on smaller screens (like laptops)
