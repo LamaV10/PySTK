684074d2aece29cb4805e5b8ba16351d65a90a6a
LamaV10 <lama.customs@gmail.com>
2024-09-24 21:44:43 +0200

    Simplified play.sh => removed unnecesarry lines.

--- play.sh
+++ play.sh
@@ -1,13 +1,13 @@
 echo "Singleplayer (1) or Multiplayer (2)?"
 read mode
 
+cd Source/
+
 if [ $mode -eq 1 ]; then
-  cd Source/
   python PySTK_main.py
 fi
 
 
 if [ $mode -eq 2 ]; then
-  cd Source/
   python PySTK_main_duo.py
 fi
