8c1023ad710d30e7031a4c142255a1e13b5007d1
LamaV10 <lama.customs@gmail.com>
2024-11-10 19:34:54 +0100

    Compiled version was addded. Hopefully a little speed improvment:)

---
+++ Source/bin/music.py
@@ -0,0 +1,15 @@
+import pygame
+from pygame import mixer
+pygame.init()
+
+def music():
+
+    run = int(input("Do you have your music setup? Yes (1) No(2): "))
+    if run == 1:
+        # music import/play
+        # change your path to your music here:
+        mixer.music.load("Music/Flashing-Lights.mp3")
+        
+
+        mixer.music.set_volume(0.5)
+        mixer.music.play(-1)
--- play.sh
+++ play.sh
@@ -1,13 +1,13 @@
 echo "Singleplayer (1) or Multiplayer (2)?"
 read mode
 
-cd Source/
+cd Source/bin
 
 if [ $mode -eq 1 ]; then
-  python PySTK_main.py
+  python PySTK_main.cpython-312.pyc
 fi
 
 
 if [ $mode -eq 2 ]; then
-  python PySTK_main_duo.py
+  python PySTK_main_duo.cpython-312.pyc
 fi
