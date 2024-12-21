b38d850ca8287fcb87d2dd6b765893a6244e6556
LamaV10 <lama.customs@gmail.com>
2024-12-21 12:02:19 +0100

    dev.sh. Comments further explaining what this script does. (PySTK: 521 L.)

--- dev.sh
+++ dev.sh
@@ -1,3 +1,6 @@
+# This script compiles PySTK_main and overwrites the old binary in the Source/bin directory.
+# Then it executes the binary (should work on Linux and maybe MacOS) 
+
 cd Source/
 python -m py_compile PySTK_main.py
 mv -f __pycache__/PySTK_main.cpython-312.pyc bin/
