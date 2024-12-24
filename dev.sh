ea79b0ce8b96d802966e7ba9627a87fa68a54506
LamaV10 <lama.customs@gmail.com>
2024-12-24 15:07:00 +0100

    Little addition to dev.sh in order to make it compatible to MacOs, because the python command there is python3.

--- dev.sh
+++ dev.sh
@@ -1,10 +1,17 @@
 # This script compiles PySTK_main and overwrites the old binary in the Source/bin directory.
 # Then it executes the binary (should work on Linux and maybe MacOS) 
 
+if [ -f /usr/bin/python]; then
+  pythonTyp=python
+
+elif [ -f /usr/bin/python3]; then
+  pythonTyp=python3
+fi
+
 cd Source/
-python -m py_compile PySTK_main.py
+pythonTyp -m py_compile PySTK_main.py
 mv -f __pycache__/PySTK_main.cpython-313.pyc bin/
 cd bin/
-python PySTK_main.cpython-313.pyc
+pythonTyp PySTK_main.cpython-313.pyc
 cd ..
 cd ..
