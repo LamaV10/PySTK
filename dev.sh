7acd4b7c54b0f1503a1a137ef2c72fa8c838c162
LamaV10 <lama.customs@gmail.com>
2024-12-24 15:14:46 +0100

    Forget the dollar sign and the right spacing.

--- dev.sh
+++ dev.sh
@@ -1,17 +1,17 @@
 # This script compiles PySTK_main and overwrites the old binary in the Source/bin directory.
 # Then it executes the binary (should work on Linux and maybe MacOS) 
 
-if [ -f /usr/bin/python]; then
+if [ -f /usr/bin/python ]; then
   pythonTyp=python
 
-elif [ -f /usr/bin/python3]; then
+elif [ -f /usr/bin/python3 ]; then
   pythonTyp=python3
 fi
 
 cd Source/
-pythonTyp -m py_compile PySTK_main.py
+$pythonTyp -m py_compile PySTK_main.py
 mv -f __pycache__/PySTK_main.cpython-313.pyc bin/
 cd bin/
-pythonTyp PySTK_main.cpython-313.pyc
+$pythonTyp PySTK_main.cpython-313.pyc
 cd ..
 cd ..
