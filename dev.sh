dbc5ca67d33336af0f6b4e267a52e81513f3a454
LamaV10 <lama.customs@gmail.com>
2024-12-24 15:20:48 +0100

    Ugly fix for different python versions. 3.12 and 3.13 should now be supported.

--- dev.sh
+++ dev.sh
@@ -1,17 +1,19 @@
 # This script compiles PySTK_main and overwrites the old binary in the Source/bin directory.
 # Then it executes the binary (should work on Linux and maybe MacOS) 
 
-if [ -f /usr/bin/python ]; then
-  pythonTyp=python
+if [ -f /usr/bin/python3.13 ]; then
+  pythonTyp=python3.13
+  file=PySTK_main.cpython-313.pyc
 
-elif [ -f /usr/bin/python3 ]; then
-  pythonTyp=python3
+elif [ -f /usr/bin/python3.12 ]; then
+  pythonTyp=python3.12
+  file=PySTK_main.cpython-312.pyc
 fi
 
 cd Source/
 $pythonTyp -m py_compile PySTK_main.py
-mv -f __pycache__/PySTK_main.cpython-313.pyc bin/
+mv -f __pycache__/$file bin/
 cd bin/
-$pythonTyp PySTK_main.cpython-313.pyc
+$pythonTyp $file 
 cd ..
 cd ..
