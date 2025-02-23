f52b4a8041e23ec689451af8c8145c2f8cc9e7ea
LamaV10 <lama.customs@gmail.com>
2025-02-23 19:56:22 +0100

    play.sh update. Python 3.11 was added, because some old systems still have it installed.

--- play.sh
+++ play.sh
@@ -1,3 +1,4 @@
+# searches which python version is installed (from 3.13 - 3.11)
 if [ -f /usr/bin/python3.13 ]; then
   pythonTyp=python3.13
   file=PySTK_main.cpython-313.pyc
@@ -5,9 +6,14 @@ if [ -f /usr/bin/python3.13 ]; then
 elif [ -f /usr/bin/python3.12 ]; then
   pythonTyp=python3.12
   file=PySTK_main.cpython-312.pyc
+
+elif [ -f /usr/bin/python3.11 ]; then
+  pythonTyp=python3.12
+  file=PySTK_main.cpython-311.pyc
+
 fi
 
 tag="-m"
 
 cd Source/bin/
-$pythonTyp $file 
+$pythonTyp $file
