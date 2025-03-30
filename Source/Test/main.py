8fc7dddafdcc0f345c297565dcfb429a4a33f659
LamaV10 <lama.customs@gmail.com>
2025-03-30 21:30:27 +0200

    Seperate file for variables almost ready. The only issue is with the scale_factor. If you hard code the number it works.

---
+++ Source/Test/main.py
@@ -0,0 +1,20 @@
+from testModule import *
+
+pipi = "pipi"
+kaka = pipi + "kaka"
+
+# print(kaka)
+print(pups)
+
+bruno = [0] 
+def fortnite():
+    bruno[0] += 1
+fortnite()
+print(bruno[0])
+
+banani = 0
+def minecraft():
+    global banani
+    banani = 5 
+minecraft()
+print(banani)
